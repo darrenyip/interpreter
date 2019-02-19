e32893809ae7fc5759e33911236d1d1f01ef39ba
Zhangyu Ye <Darrenyip>
2019-02-19 14:05:12 -0500

    update

--- printer.c
+++ printer.c
@@ -2,10 +2,18 @@
 #include <stdio.h>
 
 void printOutput(int arr[]){
-    printf("works?\n");
-    for(int i; i<20;i++){
-        printf("works2?\n");
-        printf("%c",arr[i]);
+    // for(int i; i<20;i++){
+    //     printf("works2?\n");
+    //     printf("%c",arr[i]);
+    // }
+    int i,*ptr;
+    ptr = arr;
+    printf("Printer output = ");
+    for(int i =0;i<20;i++){
+        printf("%c",*ptr);
+      /* move to the next location */
+        ptr++;
     }
     printf("\n");
+    printf("50 = %c\n",50);
 }
--- rpn.c
+++ rpn.c
@@ -12,37 +12,44 @@ void toPostfix(){
     globalStack = NULL;         //create the stack
     int c = 0,counter = 0;                  //counter for looping the line string
     _bool checker = FALSE; //check if the next character is the number
-    int preCh = line[0];
-    int curCh;
+    int preCh;
+    int* ptr;
+    ptr = output;
     while(line[c]!= '\0'){
         int ch = line[c]; //for simple codeing
+        if(c>0) preCh = line[c-1];
+        if(((ch<=57)&&(ch>=48))&&((preCh<=57)&&(preCh>=48))){
+            checker = TRUE;
+        }
         if(checker){
-            output[counter] = output[counter]*10 + ch;
+            printf("check is true and %c is added to ouput\n",ch);
+            --ptr;
+            *ptr = (int)(*ptr)*10 + ch;
+            ++ptr;
+            checker = FALSE;
         }else if(ch<48 || ch>57 ){
             if(ch == 41){
                 printf("This ( symbol is found and poped\n");
                 int popOut = (int)pop(&globalStack);
                 char popP = pop(&globalStack);
                 printf("previous %c is poped\n",popP);
-                output[counter] = popOut;
+                *ptr = popOut;
+                ++ptr;
                 counter++;
             }else{
             push(&globalStack,ch);
             }
         }else{
             printf("number %c is added to output array\n",ch);
-            output[counter] = ch;
+            // output[counter] = ch;
+            *ptr = ch;
+            ++ptr;
             counter++;
         }
         // printf("%c\n",line[c]);
         c++;
-        curCh = line[c];
-        if(((curCh<=57)&&(curCh>=48))&&((preCh<=57)&&(preCh>=48))){
-            checker = TRUE;
-        }
+        
     }
-    printf("here\n");
     printOutput(output);
-    printf("here2\n");
 
 }
